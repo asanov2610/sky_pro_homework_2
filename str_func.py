6c259f4f99d7635db77badcb854762faec2ea293
Arlen Asanov <a.asanov2610@gmail.com>
2023-07-25 12:55:31 +0300

    add new func get_tittle

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def get_upper_text(text):
     """Функция возвращает текст с заглавными буквами"""
     return str.upper(text)
+
+def get_tittle_text(text):
+    """Функция возвращает текст с первой заглавной буквой"""
+    return str.title(text)
